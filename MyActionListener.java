d36463ee6c31e361e4bea7dcb66e92a456c33a53
DivyarajChudasama <92981225+DivyarajChudasama@users.noreply.github.com>
2022-06-28 11:47:26 +0530

    .

--- MyActionListener.java
+++ MyActionListener.java
@@ -1,4 +1,3 @@
-
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
@@ -6,131 +5,44 @@ import javax.swing.*;
 import java.lang.*;
 import java.util.*;
 
-public class MyActionListener implements ActionListener, ItemListener
-{
+public class MyActionListener implements ItemListener, ActionListener
+{	
+	int i;
 	MyFrame mf;
-	MyFrame1 mf1;
-	MyFrame2 mf2;
-	MyFrame4 mf4;
+	ArrayList <String> al;
 	
-	
-	MyActionListener(MyFrame m)
-    {
+	MyActionListener(MyFrame m){
 		this.mf = m;
-	}
-
-    	MyActionListener(MyFrame1 m)
-    {
-		this.mf1 = m;
-
-	}
-		MyActionListener(MyFrame2 m)
-    {
-		this.mf2 = m;
-	}
-		MyActionListener(MyFrame4 m)
-    {
-		this.mf4 = m;
+		al = new ArrayList<String>();
 	}
 	
-	static String a1;
-	static String a2;
-	static String a3;
-	static String a4;
-	static String a5;
-
-	public void itemStateChanged(ItemEvent e)
+	public void itemStateChanged(ItemEvent e) 
 	{
-
-		if(mf2.comboBox1.getSelectedItem().equals("Ahmedabad")) //&& mf2.comboBox3.getSelectedItem().equals("Surat")//
+		if(e.getStateChange()==e.SELECTED)
 		{
-			System.out.println("sfhfsdfhdfgdhd");  
+			al.add(e.getItem().toString());
 			
 		}
-
-	}
-	public void actionPerformed(ActionEvent e)
-	{
-		if(e.getActionCommand().equals("NEXT"))
-        {
-            mf1 = new MyFrame1();
-            this.mf1.setVisible(true);	
-			
-        }
-
-		if(e.getActionCommand().equals("NEXT1"))
-        {
-			//  mf1 = new MyFrame1();
-
-            mf2 = new MyFrame2();
-            this.mf2.setVisible(true);	
-
-        }
-
-		if(e.getActionCommand().equals("SAVE1"))
+		else
 		{
-			a1 = this.mf1.t1.getText();
-			a2 = this.mf1.t2.getText();
-			a3 = this.mf1.t3.getText();
-			a4 = this.mf1.t4.getText();
-			a5 = this.mf1.t5.getText();
-			
-			try
-			{
-				FileWriter fw = new FileWriter("data.txt");
-				fw.write(a1);
-				fw.write("\n");
-				fw.write(a2);
-				fw.write("\n");
-				fw.write(a3);
-				fw.write("\n");
-				fw.write(a4);
-				fw.write("\n");
-				fw.write(a5);
-							
-				fw.close();
-			}
-			catch(Exception ex)
-			{
-				System.out.println(ex);
-
-			}
+			al.remove(e.getItem().toString());
 		}
-
-		if(e.getActionCommand().equals("NEXT2"))
-        {
-            mf4 = new MyFrame4();
-            this.mf4.setVisible(true);	
-        }
-
-		if(e.getActionCommand().equals("PREVIOUS1"))
-        {
-            mf = new MyFrame();
-            this.mf.setVisible(true);	
-        }
-
-		if(e.getActionCommand().equals("PREVIOUS2"))
-        {
-            mf1 = new MyFrame1();
-            this.mf1.setVisible(true);	
-        }
-
-		if(e.getActionCommand().equals("PREVIOUS3"))
-        {
-            mf2 = new MyFrame2();
-            this.mf2.setVisible(true);	
-        }
-
-
-        }
-
-
-
-		
-
-		
-
-
-
+	}
+	
+	public void actionPerformed(ActionEvent ae)
+	{
+		if(i<=2)
+		{
+			for(String s:al)
+		{
+			System.out.println(s);
+		}
+		}else{
+			System.out.println("")
+		}
 		
 	}
+
+
+	
+}
--- MyFrame.java
+++ MyFrame.java
@@ -6,63 +6,57 @@ import java.lang.*;
 
 public class MyFrame extends Frame
 {
-	TextField t1,t2,t3;
-	Label l1,l2,l3,l4;
-	Button b1,b2,b3;
+	Button b1;
+	Checkbox c1, c2, c3,c4,c5,c6;
 	Font f1;
-	
-	//MyActionListener ml = new MyActionListener(this);
+	MyActionListener ml = new MyActionListener(this);
 	MyFrame()
 	{
-		super("Election System");
+		super("My Test Application");
 		setLayout(null);
-		setBounds(0,0,1600,1000);
-		t1 = new TextField();
-		t2 = new TextField();
-		t3 = new TextField();
-		b1 = new Button("SAVE");
-		b2 = new Button("CLEAR");
-		b3 = new Button("NEXT");
-		l1 = new Label("ELECTION SYSTEM");
-		l2 = new Label("Name");
-		l3 = new Label("Age");
-		l4 = new Label("COUNTRY");
-	
-		l1.setBounds(100,100,400,100);
-		l2.setBounds(100,300,400,100);
-		l3.setBounds(100,550,400,100);
-		l4.setBounds(100,550,400,100);
-		t1.setBounds(500,50,400,100);
-		t2.setBounds(500,300,400,100);
-		t3.setBounds(500,550,400,100);
-		b1.setBounds(600,700,200,100);
-		b2.setBounds(600,700,200,100);
-		b3.setBounds(600,700,200,100);
-		Font f1 = new Font("Arial",Font.BOLD,30);
+		setBounds(0,0,1260,1000);
 		
-		l1.setFont(f1);
-		l2.setFont(f1);
-		l3.setFont(f1);
-		l4.setFont(f1);
-		t1.setFont(f1);
-		t2.setFont(f1);
-		t3.setFont(f1);
-		b1.setFont(f1);
-		b2.setFont(f1);
-		b3.setFont(f1);
-
-		add(t1);
-		add(t2);
-		add(t3);
-		add(l1);
-		add(l2);
-		add(l3);
-		add(l4);
+		c1 = new Checkbox("Playing");
+		c2 = new Checkbox("Dancing");
+		c3 = new Checkbox("Singing");
+		c4 = new Checkbox("Dancing");
+		c5 = new Checkbox("Reading");
+		c6 = new Checkbox("Programming");
+		b1=new Button("Click");
+		
+		add(c1);
+		add(c2);
+		add(c3);
+		add(c4);
+		add(c5);
+		add(c6);
 		add(b1);
-		add(b2);
-		add(b3);
 
-		/*b1.addActionListener(ml);	
+		c1.setBounds(100,100,400,100);
+		c2.setBounds(100,200,400,100);
+		c3.setBounds(100,300,400,100);
+		c4.setBounds(100,400,400,100);
+		c5.setBounds(100,500,400,100);
+		c6.setBounds(100,600,400,100);
+		b1.setBounds(100,800,400,100);
+		
+		f1 = new Font("Times",Font.BOLD,50);
+		
+		c1.setFont(f1);
+		c2.setFont(f1);
+		c3.setFont(f1);
+		c4.setFont(f1);
+		c5.setFont(f1);
+		c6.setFont(f1);
+		
+		c1.addItemListener(ml);
+		c2.addItemListener(ml);
+		c3.addItemListener(ml);
+		c4.addItemListener(ml);
+		c5.addItemListener(ml);
+		c6.addItemListener(ml);
+		
+		b1.addActionListener(ml);
 		addWindowListener(new WindowAdapter()
 			{
 				public void windowClosing(WindowEvent we)
@@ -70,14 +64,6 @@ public class MyFrame extends Frame
 					System.exit(0);
 				}
 			});
-		b2.addActionListener(ml);	
-		addWindowListener(new WindowAdapter()
-			{
-				public void windowClosing(WindowEvent we)
-				{
-					System.exit(0);
-				}
-			});*/
 	}
 	public static void main(String args[])
 	{
